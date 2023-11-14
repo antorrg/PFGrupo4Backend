c6ba9ac35c19793da3c40ba4fe35bd527e9bac95
antorrg <114789901+antorrg@users.noreply.github.com>
2023-11-14 01:03:41 -0300

    Primer commit

---
+++ api/README.md
@@ -0,0 +1,7 @@
+# GameWorld Server
+Este servidor cuenta con la instalacion de las siguientes dependencias:  express, axios, dotenv, nodemon, morgan.
+
+Para inicializar en desarrollo el comando es: ```npm run dev```, esto inicializa con nodemon.
+El comando ```npm start``` también va a inicializar pero con node, a cada cambio lo deberiamos reiniciar manualmente, pero a la hora de deployarlo es necesario que esté así.
+
+## ¡Exitos muchachos!
---
+++ api/index.js
@@ -0,0 +1,20 @@
+//?   dP""b8    db    8b    d8 888888 Yb        dP  dP"Yb  88""Yb 88     8888b.  
+//?  dP   `"   dPYb   88b  d88 88__    Yb  db  dP  dP   Yb 88__dP 88      8I  Yb 
+//?  Yb  "88  dP__Yb  88YbdP88 88""     YbdPYbdP   Yb   dP 88"Yb  88  .o  8I  dY 
+//?   YboodP dP""""Yb 88 YY 88 888888    YP  YP     YbodP  88  Yb 88ood8 8888Y"  
+
+    //todo   .d  dP"Yb     dP   .d   .d    dP oP"Yb.  dP"Yb  oP"Yb. 88888 
+    //todo .d88 dP   Yb   dP  .d88 .d88   dP  "' dP' dP   Yb "' dP'   .dP 
+    //todo   88 Yb   dP  dP     88   88  dP     dP'  Yb   dP   dP'  o `Yb 
+    //todo   88  YbodP  dP      88   88 dP    .d8888  YbodP  .d8888 YbodP 
+//-----------------------------------------------------------------------------------------
+
+const server=require('./src/server');
+
+require('dotenv').config();
+const {PORT}=process.env;
+
+
+server.listen(PORT, ()=>{
+    console.log(`Server is running in ${PORT} ✔️`)
+});
