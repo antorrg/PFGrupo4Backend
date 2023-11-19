9bb332c6c6e39923b339a21021d7f903efe6d732
Vlad <vladimir.esquivel@timba.co>
2023-11-19 16:44:22 -0500

    Se agregon las propiedades gendersText y platformsText a los datos que retorna el endpoint Videogames

--- api/src/Controllers/getVideogames.js
+++ api/src/Controllers/getVideogames.js
@@ -33,8 +33,42 @@ const getVideogames = async (page, size, filters, platformsFilters, genresFilter
             ],
             limit: size,
             offset: page*size,
-            distinct:true
+            distinct: true
         });
+
+        let videogamesData = [];
+        if(count) {
+            videogamesData = rows.map((game) => {
+                let auxGame = {...game.get()}; //En el contexto de Sequelize, el método .get() se utiliza para obtener una representación simple del modelo, excluyendo las propiedades y métodos internos de Sequelize. Cuando se realiza una consulta con Sequelize, el resultado incluye instancias del modelo, y el método .get() se utiliza para obtener un objeto plano que representa los datos del modelo sin las propiedades y métodos internos de Sequelize.
+
+                auxGame.Genres = game.Genres.map((auxGenre) => {
+                    return auxGenre.name;
+                });
+                auxGame.Platforms = game.Platforms.map((auxGenre) => {
+                    return auxGenre.name;
+                });
+
+                auxGame.genresText = auxGame.Genres.join(", ");
+                auxGame.platformsText = auxGame.Platforms.join(", ");
+
+                return auxGame;
+                /*return {
+                    ...game.get(),
+                    genresText: game.Genres.map((auxGenre) => {
+                        return auxGenre.name;
+                    }).join(", "),
+                    platformsText: game.Platforms.map((auxPlatf) => {
+                        return auxPlatf.name;
+                    }).join(", "),
+                    Genres: game.Genres.map((auxGenre) => {
+                        return auxGenre.name;
+                    }),
+                    Platforms: game.Platforms.map((auxGenre) => {
+                        return auxGenre.name;
+                    })
+                };*/
+            });
+        }
         
         const auxTotalPages = Math.ceil(count / size);
         const auxPrevPage = (page - 1) >= 0 ? (page - 1) : -1;
@@ -43,7 +77,7 @@ const getVideogames = async (page, size, filters, platformsFilters, genresFilter
         const auxHasNextPage = (page + 1) < auxTotalPages ? true : false;
         
         return {
-            videogames: rows,
+            videogames: videogamesData,
             PaginationData: {
                 totalItems: count,
                 limit: size,
