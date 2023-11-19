f87be49d0890723c0bda5b5e0e491fd24156a7b1
Vlad <vladimir.esquivel@timba.co>
2023-11-18 20:15:01 -0500

    Endpoint Videojuegos con filtro, busqueda por nombre y paginado

--- api/src/Controllers/getVideogames.js
+++ api/src/Controllers/getVideogames.js
@@ -1,17 +1,45 @@
 const { Videogame, Genre, Platform} = require("../database");
+const { Op } = require("sequelize");
 
-const getVideogames = async (page, size, req, res) => {
+const getVideogames = async (page, size, filters, platformsFilters, genresFilters, req, res) => {
+
+    page = +page;
+    size = +size;
 
     try {
         const { count, rows } = await Videogame.findAndCountAll({
+            where: filters,
+            include: [
+                {
+                    model: Genre,
+                    attributes: ["name"],
+                    where: genresFilters,
+                    /*where: {
+                        //name: ["Actions", "Puzzle", "Indie"]
+                        name: { [Op.in]: ["Actions", "Puzzle", "Indie"] }
+                    },*/
+                    through: {
+                        attributes: []
+                    }
+                },
+                {
+                    model: Platform,
+                    attributes: ["name"],
+                    where: platformsFilters,
+                    through: {
+                        attributes: []
+                    }
+                }
+            ],
             limit: size,
-            offset: page*size
+            offset: page*size,
+            distinct:true
         });
         
         const auxTotalPages = Math.ceil(count / size);
-        const auxPrevPage = (page - 1) > 0 ? (page - 1) : -1;
-        const auxNextPage = (page + 1) < auxTotalPages ? (page + 1) : -1;
-        const auxHasPrevPage = (page - 1) > 0 ? true : false;
+        const auxPrevPage = (page - 1) >= 0 ? (page - 1) : -1;
+        const auxNextPage = (page + 1) <= auxTotalPages ? (page + 1) : -1;
+        const auxHasPrevPage = (page - 1) >= 0 ? true : false;
         const auxHasNextPage = (page + 1) < auxTotalPages ? true : false;
         
         return {
