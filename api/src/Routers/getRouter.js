0b71a8ac34ce199a14c53c7b3e0b4df23b62f0dc
antorrg <114789901+antorrg@users.noreply.github.com>
2023-12-11 23:21:50 -0300

    commit durante la daily

--- api/src/Routers/getRouter.js
+++ api/src/Routers/getRouter.js
@@ -19,11 +19,11 @@ const getRatedByItemIdHandler = require("../Handlers/Payments/getRatedByItemIdHa
 //======================================================================================
 getRouter.get("/videogames", getVideogamesHandler);//Modulos games/videogames (Libres)
 getRouter.get("/games/:id", getDetailHandler); //Modulos games/videogames (Libres)
-getRouter.get("/genres", verifyToken, getGenresHandler); //Protegida
-getRouter.get("/platforms", verifyToken,getPlatformHandler); //Protegida
-getRouter.get("/games", verifyToken,getGamesAdminHandler); //Libres
+getRouter.get("/genres", getGenresHandler); //Protegida
+getRouter.get("/platforms", getPlatformHandler); //Protegida
+getRouter.get("/games", getGamesAdminHandler); //Libres
 getRouter.get("/user", verifyToken, getUserHandler) //Protegida
-getRouter.get("/user/:id", getUserDetailHand) //Libre
+getRouter.get("/user/:id", verifyToken, getUserDetailHand) //Libre
 
 //Payments:
 getRouter.get("/getParchuseOrder", getParchuseOrderHandler);
