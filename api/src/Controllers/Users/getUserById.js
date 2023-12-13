667812b836e6f945d2d42ae2d82939ef55db40a3
antorrg <114789901+antorrg@users.noreply.github.com>
2023-12-13 15:42:36 -0300

    implementando SU

--- api/src/Controllers/Users/getUserById.js
+++ api/src/Controllers/Users/getUserById.js
@@ -12,5 +12,18 @@ const getUserById = async (id) => {
       throw new Error({ error: error.message });
     }
   };
+
+  const getAllUser = async () => {
+    try {
+      const infodb = await User.findAll();
+      if (!infodb || infodb.deleteAt === true) {
+        throw new Error("User not found");
+      }
+     return infodb;
+      
+    } catch (error) {
+      throw new Error({ error: error.message });
+    }
+  };
   
-  module.exports=getUserById;
+  module.exports={getUserById, getAllUser};
--- api/src/Routers/getRouter.js
+++ api/src/Routers/getRouter.js
@@ -11,11 +11,12 @@ const getVideogamesHandler = require("../Handlers/VideoGames/getVideogamesHandle
 const getParchuseOrderHandler = require("../Handlers/Payments/getParchuseOrderHandler");
 const getDetailHandler= require('../Handlers/VideoGames/getDetailHandler')
 const verifyToken= require('../utils/verifyToken');
-const getUserDetailHand = require("../Handlers/Users/detailUserHandler");
+const {getUserDetailHand, getUserAllHand} = require("../Handlers/Users/detailUserHandler");
 const getUserShoppingCartHandler = require("../Handlers/Users/getUserShoppingCartHandler");
 const getOrdersByUserIdHandler = require("../Handlers/Payments/getOrdersByUserIdHandler");
 const getRatedPendingItemsByUserIdHandler = require("../Handlers/Payments/getRatedPendingItemsByUserIdHandler");
 const getRatedByItemIdHandler = require("../Handlers/Payments/getRatedByItemIdHandler");
+const {validUserSu} = require('../utils/validateUsers')
 //======================================================================================
 getRouter.get("/videogames", getVideogamesHandler);//Modulos games/videogames (Libres)
 getRouter.get("/games/:id", getDetailHandler); //Modulos games/videogames (Libres)
@@ -24,6 +25,8 @@ getRouter.get("/platforms", getPlatformHandler); //Protegida
 getRouter.get("/games", getGamesAdminHandler); //Libres
 getRouter.get("/user", verifyToken, getUserHandler) //Protegida
 getRouter.get("/user/:id", verifyToken, getUserDetailHand) //Libre
+//===================================================================
+getRouter.get("/usersu",   validUserSu , getUserAllHand) //Protegida
 
 //Payments:
 getRouter.get("/getParchuseOrder", getParchuseOrderHandler);
