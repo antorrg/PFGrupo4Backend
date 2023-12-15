5d1a64eb79b288e59462a4b95ad3856a83db2ce4
antorrg <114789901+antorrg@users.noreply.github.com>
2023-12-15 18:59:19 -0300

    Update putRouter.js

--- api/src/Routers/putRouter.js
+++ api/src/Routers/putRouter.js
@@ -4,11 +4,11 @@ const putRouter = Router();
 const {gameUpdaterHand,genreUpdaterHand,platformUpdaterHand} = require("../Handlers/Admin/gameUpdaterHand");
 const {userUpdaterHand,userSUpdaterHand} = require('../Handlers/Users/userUpdaterHand');
 const verifyToken= require('../utils/verifyToken')
-const {validUserSu} = require('../utils/validateUsers')
+const {validUserSu,verifyUsPas} = require('../utils/validateUsers')
 
 //====================================================================================
 putRouter.put("/games/:id", verifyToken, gameUpdaterHand); //Modulo games/videogames
-putRouter.put('/user/:id', verifyToken, userUpdaterHand); //Modulo user
+putRouter.put('/user/:id', verifyToken, verifyUsPas, userUpdaterHand); //Modulo user
 putRouter.put('/genre/:id', verifyToken, genreUpdaterHand); //Modulo genre
 putRouter.put('/platform/:id', verifyToken, platformUpdaterHand); //Modulo platform
 putRouter.put("/usersu/:id",   validUserSu , userSUpdaterHand) //Protegida
