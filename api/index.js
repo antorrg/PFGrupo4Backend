e371dcc6972ab1d164b85006190318232783c46d
Antonio R. Rodriguez Gramajo <114789901+antorrg@users.noreply.github.com>
2024-02-21 22:42:59 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ const { PORT } = process.env;
 
 server.listen(PORT, async () => {
   try {
-    await sequelize.sync({ force:false});
+    await sequelize.sync({ force:true});
     await fillTables();
     await appUserTable();
     console.log(`Server is running on port ${PORT} ✔️`);
